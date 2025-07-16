fbb4e50b6c88e55734f244f2eb56e7ba40938ede
kaishaoshao <2032751077@qq.com>
2025-07-16 13:52:20 +0800

    [include]: 添加stdio.h和stdlib.h的头文件,并且做好部分函数声明

---
+++ include/stdio.h
@@ -0,0 +1,62 @@
+#ifndef __LIBC_STDLIB_H__
+#define __LIBC_STDLIB_H__
+
+//  file end or error number
+#undef  EOF
+#define EOF (-1)
+
+// NULL ptr
+#undef  NULL
+#define NULL ((void *)0)
+
+// Buffer size
+#define BUFSIZ 8192
+
+typedef struct _IO_FILE FILE;
+
+extern FILE *stdin;
+extern FILE *stdout;
+extern FILE *stderr;
+
+// --- File operation ---
+FILE *fopen(const char *filename, const char *mode);
+int   fclose(FILE *stream);
+int   fflush(FILE *stream);
+
+// --- Format input/output ----
+int printf(const char *format, ...);
+int fprintf(FILE *stream, const char *format, ...);
+int sprintf(char *str, const char *format, ...);
+int snprintf(char *str, size_t n, const char *format, ...);
+
+int scanf(const char *format, ...);
+int fscanf(FILE *stream, const char *format, ...);
+
+// --- Character input/output ---
+int getchar(void);
+int fgetc(FILE *stream);
+
+int putchar(int character);
+int fputc(int character, FILE *stream);
+
+// --- String input/output ---
+char *fgets(char *str, int n, FILE *stream);
+char *gets(char *str);
+
+int   fputs(const char *str, FILE *stream);
+
+// --- Binary (direct) input/output ---
+size_t fread(void *ptr, size_t size, size_t count, FILE *stream);
+size_t fwrite(const void *ptr, size_t size, size_t count, FILE *stream);
+
+// --- File location ---
+int  fseek(FILE *stream, long offset,int origin);
+long ftell(FILE *stream);
+void rewind(FILE *stream);
+
+// --- Error handling ---
+void perror(const char *s);
+int  feof(FILE *stream);
+int  ferror(FILE *stream);
+
+#endif // __LIBC_STDLIB_H__
---
+++ include/stdlib.h
@@ -0,0 +1,41 @@
+#ifndef __LIBC_STDLIB_H__
+#define __LIBC_STDLIB_H__
+
+typedef unsigned long size_t;
+
+#define EXIT_SUCCESS 0
+#define EXIT_FAILURE 1
+
+#define RAND_MAX 32767
+
+// --- String conversion ---
+double atof(const char *str);
+int    atoi(const char *str);
+long   atol(const char *str);
+double strtod(const char *str, char **endptr);
+long   strtol(const char *str, char **endptr, int base);
+
+// --- Dynamic Memory management ---
+void *malloc(size_t size);
+void *calloc(size_t num, size_t size);
+void *realloc(void *ptr, size_t new_size);
+void  free(void *ptr);
+
+// --- Process control ---
+void  abort(void);
+void  exit(int status);
+int   atexit(void (*func)(void));
+int   system(const char *command);
+char *getenv(const char *name);
+
+// --- Algorithm (Search and Sorting) ---
+void *bsearch(const void *key, const void *base, size_t nitems, size_t size,
+              int (*compar)(const void *, const void *));
+void  qsort(void *base, size_t nitems, size_t size,
+           int (*compar)(const void *, const void *));
+
+// --- Integer operation ---
+int  abs(int n);
+long labs(long n);
+
+#endif // __LIBC_STDLIB_H__
